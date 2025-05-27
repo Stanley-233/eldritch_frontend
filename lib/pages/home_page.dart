458b59f65d4ec7b19e71aac23dea4f01e3726e08
54shitaimzf <mzfshitiancai@126.com>
2025-05-27 16:56:41 +0800

    feat: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,36 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../services/auth_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<StatefulWidget> createState() => _FunctionAreaState();
+}
+
+class _FunctionAreaState extends State<StatefulWidget>{
+  int funID = 0;
+
+  void _messageMode(){
+    setState(() {
+      funID = 0;
+    });
+  }
+
+  void _commitMode(){
+    setState(() {
+      funID = 1;
+    });
+  }
+
+  void _feedbackMode(){
+    setState(() {
+      funID = 2;
+    });
+  }
+
+  void _manageMode(){
+    setState(() {
+      funID = 3;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
     final user = authService.user!;
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            tooltip: '退出登录',
-            onPressed: () async {
-              await authService.logout();
-              Navigator.pushReplacementNamed(context, '/login');
-            },
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(onPressed: _messageMode,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.purple[200]),
+                  minimumSize: WidgetStatePropertyAll(Size(100,50))
+                ), child: Text('消息')
+              ),
+              TextButton(onPressed: _commitMode,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.purple[200]),
+                  minimumSize: WidgetStatePropertyAll(Size(100,50))
+                ), child: Text('工单提交')),
+              TextButton(onPressed: _feedbackMode,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.purple[200]),
+                  minimumSize: WidgetStatePropertyAll(Size(100,50))
+                ), child: Text('反馈')),
+              TextButton(onPressed: _manageMode,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.purple[200]),
+                  minimumSize: WidgetStatePropertyAll(Size(100,50))
+                ), child: Text('管理')),
+            ],
           ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome, ${user.name}!', style: TextStyle(fontSize: 24)),
-            SizedBox(height: 10),
-            Text('Token: ${user.password}'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.logout),
+              tooltip: '退出登录',
+              onPressed: () async {
+                await authService.logout();
+                Navigator.pushReplacementNamed(context, '/login');
+              },
+            ),
           ],
         ),
-      ),
+        body: Center(
+          
+        )
     );
   }
 }
