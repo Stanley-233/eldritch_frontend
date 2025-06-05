a558f326f06159551620ead676f585808b37fc87
Stanley_233 <acc_stanley@foxmail.com>
2025-06-06 03:54:31 +0800

    fix: create message page

--- lib/pages/message_add_page.dart
+++ lib/pages/message_add_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:eldritch_frontend/models/user_group.dart';
 import 'package:eldritch_frontend/services/api_service.dart';
 import 'package:eldritch_frontend/services/auth_service.dart';
 import 'package:eldritch_frontend/services/msg_service.dart';
@@ -15,17 +16,41 @@ class MessageAddPage extends StatefulWidget {
 
 class _MessageAddPageState extends State<MessageAddPage> {
   String markdownContent = "";
+  List<bool> isChecked = [];
   List<int> needToBeSend = [];
+  List<UserGroup> groupList = []; // 存储用户组数据
+  bool isLoading = true; // 加载状态
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
     markdownContent = widget.contentController.text;
+    _loadGroupList(); // 初始化时加载用户组数据
+  }
+
+  void _loadGroupList() async {
+    try {
+      final response = await getGroupList();
+      if (response.statusCode == 200) {
+        groupList = extractGroupsFromJson(utf8.decode(response.bodyBytes));
+        isChecked = List<bool>.filled(groupList.length, false);
+      } else {
+        // 处理错误状态
+        groupList = [];
+      }
+    } catch (e) {
+      groupList = [];
+    } finally {
+      setState(() {
+        isLoading = false; // 加载完成
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    needToBeSend = [];
     return PopScope(
       child: Scaffold(
         appBar: AppBar(
@@ -42,145 +67,105 @@ class _MessageAddPageState extends State<MessageAddPage> {
           child: Center(
             child: SizedBox(
               width: (MediaQuery.of(context).size.width > 800) ? 800 : null,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: widget.titleController,
-                    decoration: const InputDecoration(
-                      labelText: '消息标题',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '消息标题不可为空';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: widget.contentController,
-                    decoration: const InputDecoration(
-                      labelText: '消息内容',
-                      border: OutlineInputBorder(),
-                    ),
-                    maxLines: 12,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '消息内容不可为空';
-                      }
-                      return null;
-                    },
-                  ),
-                  FutureBuilder(
-                      future: getUserGroups(AuthService().user!.name),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                              child: SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: CircularProgressIndicator(),
-                              )
-                          );
-                        } else if (snapshot.hasError) {
-                          return Center(
-                            child: Text('无法连接到服务器'),
-                          );
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: widget.titleController,
+                      decoration: const InputDecoration(
+                        labelText: '消息标题',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return '消息标题不可为空';
                         }
-                        else {
-                          final response = snapshot.data!;
-                          final groupList = extractGroupsFromJson(utf8.decode(
-                              response.bodyBytes));
-                          if (response.statusCode != 200) {
-                            if (response.statusCode == 404) {
-                              return Center(
-                                child: Text('用户不存在，请重新登录'),
-                              );
-                            }
-                            return Center(
-                              child: Text("服务器内部错误"),
-                            );
-                          }
-                          if (groupList.isEmpty) {
-                            return Center(
-                              child: Text('暂无用户组'),
-                            );
-                          }
-                          return ListView.separated(
-                              itemBuilder: (BuildContext context, int index) {
-                                return CheckboxListTile(
-                                    title: Text(groupList[index].groupName),
-                                    value: false,
-                                    onChanged: (bool? value) {
-                                      if (value!) {
-                                        needToBeSend.add(
-                                            groupList[index].groupId);
-                                      }
-                                      else {
-                                        needToBeSend.remove(
-                                            groupList[index].groupId);
-                                      }
-                                    }
-                                );
-                              },
-                              itemCount: groupList.length,
-                              separatorBuilder: (BuildContext context,
-                                  int index) {
-                                return Divider(
-                                  height: 2.0,
-                                  color: Colors.transparent,
-                                );
-                              }
-                          );
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: widget.contentController,
+                      decoration: const InputDecoration(
+                        labelText: '消息内容',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: 12,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return '消息内容不可为空';
                         }
-                      }
-                  ),
-                  const SizedBox(height: 10),
-                  /*const Text(
-                    "消息预览",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: (MediaQuery.of(context).size.width > 400) ? 400 : null,
-                    height: 300,
-                    child: Markdown(
-                      data: widget.contentController.text,
-                      styleSheet: MarkdownStyleSheet(
-                        p: const TextStyle(fontSize: 16, height: 1.5),
-                        h1: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                        h2: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                        h3: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                        code: const TextStyle(fontFamily: 'Courier', fontSize: 14),
+                        return null;
+                      },
+                    ),
+                    isLoading ? Center(
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: CircularProgressIndicator(),
+                      ),
+                    ) : groupList.isEmpty ? Center(
+                      child: Text('暂无用户组'),
+                    ) : SizedBox(
+                      width: MediaQuery.of(context).size.width > 800 ? 800 : MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      child: ListView.separated(
+                        itemBuilder: (BuildContext context, int index) {
+                          return CheckboxListTile(
+                            title: Text(groupList[index].groupName),
+                            value: isChecked[index],
+                            onChanged: (bool? value) {
+                              setState(() {
+                                isChecked[index] = value!;
+                                if (value) {
+                                  needToBeSend.add(groupList[index].groupId);
+                                } else {
+                                  needToBeSend.remove(groupList[index].groupId);
+                                }
+                              });
+                            },
+                          );
+                        },
+                        itemCount: groupList.length,
+                        separatorBuilder: (BuildContext context, int index) {
+                          return Divider(
+                            height: 2.0,
+                            color: Colors.transparent,
+                          );
+                        },
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        markdownContent = widget.contentController.text;
-                      });
-                    },
-                    child: Text("刷新 Markdown 预览"),
-                  )*/
-                ],
-              ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              )
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              postMessage(
-                PostMessageRequest(
-                  title: widget.titleController.text,
-                  content: widget.contentController.text,
-                  createdBy: AuthService().user!.name,
-                  accessGroupIds: needToBeSend
-                )
+          onPressed: () async {
+            if (_formKey.currentState?.validate() != true) {
+              return;
+            }
+            final code = await postMessage(
+              PostMessageRequest(
+                title: widget.titleController.text,
+                content: widget.contentController.text,
+                createdBy: AuthService().user!.name,
+                accessGroupIds: needToBeSend,
+              ),
+            );
+            if (code == 200) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('消息发送成功')),
               );
-            });
+              Navigator.pop(context);
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('消息发送失败，错误代码：$code')),
+              );
+            }
           },
           tooltip: "发送消息",
           child: const Icon(Icons.send),
