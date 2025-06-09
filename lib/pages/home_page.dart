f1375372f04c57a3dea2c60c868610590a4d87c8
Stanley_233 <acc_stanley@foxmail.com>
2025-06-09 23:33:34 +0800

    feat: add timer update to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:eldritch_frontend/pages/manage_page.dart';
 import 'package:eldritch_frontend/pages/message_list_page.dart';
 import 'package:eldritch_frontend/pages/order_assigned_list_page.dart';
@@ -15,10 +17,37 @@ class HomePage extends StatefulWidget {
 class _FunctionAreaState extends State<HomePage> {
   final ValueNotifier<int> funID = ValueNotifier(0);
   final _switchPage = PageController();
+  late Timer _timer;
+
+  late OrderListPage _orderListPage;
+  late OrderAssignedListPage _orderAssignedListPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _updatePages();
+    _timer = Timer.periodic(const Duration(seconds: 30), (timer) {
+      setState(() {
+        _updatePages();
+      });
+    });
+  }
+
+  void _updatePages() {
+    _orderListPage = OrderListPage(
+      openOrders: getCreateUserOrders("open"),
+      rejectOrders: getCreateUserOrders("reject"),
+      closedOrders: getCreateUserOrders("closed"),
+    );
+    _orderAssignedListPage = OrderAssignedListPage(
+      openOrders: getAssignedUserOrders("open"),
+      rejectOrders: getAssignedUserOrders("reject"),
+      closedOrders: getAssignedUserOrders("closed"),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     const destinations = [
       NavigationRailDestination(
         selectedIcon: Icon(Icons.mail),
@@ -56,50 +85,47 @@ class _FunctionAreaState extends State<HomePage> {
                 onTap: () {
                   //待开发
                 },
-                child: Image.asset('assets/images/logo_rect.png')),
-            )
-          )
+                child: Image.asset('assets/images/logo_rect.png'),
+              ),
+            ),
+          ),
         ),
         destinations: destinations,
         selectedIndex: index,
         onDestinationSelected: (int indexSelected) {
           funID.value = indexSelected;
           _switchPage.jumpToPage(indexSelected);
-        });
+        },
+      );
     }
+
     return Scaffold(
       body: Row(
         children: [
           ValueListenableBuilder<int>(
             valueListenable: funID,
-            builder: (_, index, __) => buildLeftNavigation(index)),
+            builder: (_, index, __) => buildLeftNavigation(index),
+          ),
           Expanded(
             child: PageView(
               controller: _switchPage,
               children: [
                 MessageListPage(),
-                OrderListPage(
-                  openOrders: getCreateUserOrders("open"),
-                  rejectOrders: getCreateUserOrders("reject"),
-                  closedOrders: getCreateUserOrders("closed")
-                ),
-                OrderAssignedListPage(
-                    openOrders: getAssignedUserOrders("open"),
-                    rejectOrders: getAssignedUserOrders("reject"),
-                    closedOrders: getAssignedUserOrders("closed")
-                ),
+                _orderListPage,
+                _orderAssignedListPage,
                 ManagePage(),
               ],
-            )
-          )
-        ]
-      )
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   @override
-  void dispose(){
-    super.dispose();
+  void dispose() {
+    _timer.cancel();
     _switchPage.dispose();
+    super.dispose();
   }
 }
